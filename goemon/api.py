54be95c45099f5f7fec4b1eff8912e8dd77d42f3
yacchin1205 <yazawa@yzwlab.net>
2022-06-07 14:27:22 +0900

    First commit

---
+++ goemon/api.py
@@ -0,0 +1,315 @@
+import copy
+import hashlib
+import logging
+import os
+import shutil
+import tempfile
+import yaml
+
+import requests
+
+
+logger = logging.getLogger(__name__)
+
+PROPERTIES = [
+    'meta',
+    'description',
+    'script',
+    'param',
+    'paramschema',
+]
+
+META_BOOLEAN_PROPERTIES = ['public', 'importable', 'distributable']
+META_STRING_OR_NONE_PROPERTIES = ['creatorLogType', 'authorLogType']
+META_STRING_OR_EMPTY_PROPERTIES = ['title']
+
+class Task(object):
+    def __init__(self, obj=None):
+        self.obj = obj if obj is not None else {}
+        self.changed = {}
+        self.dir_files = None
+        self._download_files()
+
+    def destroy(self):
+        if not self.dir_files:
+            return
+        logger.info(f'Cleanup... {self.dir_files}')
+        shutil.rmtree(self.dir_files)
+        self.dir_files = None
+
+    @property
+    def meta(self):
+        obj = {}
+        for key in META_BOOLEAN_PROPERTIES:
+            obj[key] = self.obj.get(key, False)
+        for key in META_STRING_OR_NONE_PROPERTIES:
+            obj[key] = self.obj.get(key, 'none')
+        for key in META_STRING_OR_EMPTY_PROPERTIES:
+            obj[key] = self.obj.get(key, '')
+        return yaml.dump(obj)
+
+    @meta.setter
+    def meta(self, value):
+        obj = yaml.load(value, yaml.SafeLoader)
+        for key in META_BOOLEAN_PROPERTIES:
+            self.obj[key] = obj.get(key, False)
+            self.changed[key] = True
+        for key in META_STRING_OR_NONE_PROPERTIES:
+            self.obj[key] = obj.get(key, 'none')
+            self.changed[key] = True
+        for key in META_STRING_OR_EMPTY_PROPERTIES:
+            self.obj[key] = obj.get(key, '')
+            self.changed[key] = True
+
+    @property
+    def description(self):
+        return self.obj.get('description', '')
+
+    @description.setter
+    def description(self, value):
+        self.obj['description'] = value
+        self.changed['description'] = True
+
+    @property
+    def script(self):
+        return self.obj.get('script', '')
+
+    @script.setter
+    def script(self, value):
+        self.obj['script'] = value
+        self.changed['script'] = True
+
+    @property
+    def param(self):
+        return yaml.dump(self.obj['param']) if self.obj.get('param', None) is not None else ''
+
+    @param.setter
+    def param(self, value):
+        param = yaml.load(value, yaml.SafeLoader)
+        self._validate_param(param)
+        self.obj['param'] = param
+        self.changed['param'] = True
+
+    @property
+    def paramschema(self):
+        return yaml.dump(self.obj['paramschema']) if self.obj.get('paramschema', None) is not None else ''
+
+    @paramschema.setter
+    def paramschema(self, value):
+        paramschema = yaml.load(value, yaml.SafeLoader)
+        self._validate_paramschema(paramschema)
+        self.obj['paramschema'] = paramschema
+        self.changed['paramschema'] = True
+
+    @property
+    def files(self):
+        files_yaml = yaml.dump([self._get_file_attr(file) for file in self.obj['files']]) \
+            if self.obj.get('files', None) is not None else ''
+        files_path = [(self.dir_files, file['data']['attributes']['name'])
+                      for file in self.obj.get('files', [])]
+        return files_yaml, files_path
+
+    @files.setter
+    def files(self, value):
+        oldfiles = self.obj.get('files', [])
+        files_yaml, files_path = value
+        newfiles = []
+        newdata_files = []
+        for file in yaml.load(files_yaml, yaml.SafeLoader):
+            self._validate_file(file)
+            filepath = [os.path.join(d, f) for d, f in files_path if f == file['name']][0]
+            with open(filepath, 'rb') as f:
+                hash = hashlib.sha256(f.read()).hexdigest()
+            oldfiles_matched = [f
+                                for f in oldfiles
+                                if f['data']['attributes']['name'] == file['name'] and
+                                    f['data']['attributes']['hashSHA256'] == hash]
+            if len(oldfiles_matched) > 0:
+                logger.info(f'File properties updated: {filepath}')
+                newfile = copy.deepcopy(oldfiles_matched[0])
+                newfile['data']['attributes'].update(file)
+                newfiles.append(newfile)
+                continue
+            logger.info(f'New file: {filepath} - hash: {hash}')
+            attr = {
+                'hashSHA256': hash,
+                'size': os.path.getsize(filepath),
+                'lastModified': int(os.path.getmtime(filepath) * 1000),
+            }
+            attr.update(file)
+            newfiles.append({
+                'type': 'files',
+                'data': {
+                    'attributes': attr,
+                },
+            })
+            newdata_files.append((file['name'], hash, filepath))
+        self.obj['files'] = newfiles
+        self.obj['files_data'] = newdata_files
+        self.changed['files'] = True
+
+    def serialize_as_task(self):
+        data = {}
+        all_properties = PROPERTIES + ['files'] + META_BOOLEAN_PROPERTIES \
+            + META_STRING_OR_EMPTY_PROPERTIES + META_STRING_OR_NONE_PROPERTIES
+        for prop in all_properties:
+            if prop == 'meta':
+                continue
+            if not self.changed.get(prop, False):
+                continue
+            data[prop] = self.obj[prop]
+        return data
+
+    def get_data_files(self):
+        if 'files_data' not in self.obj:
+            return []
+        return self.obj['files_data']
+
+    def _download_files(self):
+        files = self.obj.get('files', None)
+        if files is None or len(files) == 0:
+            return
+        self.dir_files = tempfile.mkdtemp()
+        for file in files:
+            path = os.path.join(self.dir_files, file['data']['attributes']['name'])
+            path_dir, _ = os.path.split(path)
+            os.makedirs(path_dir, exist_ok=True)
+            with open(path, 'wb') as f:
+                url = file['links']['download']
+                r = requests.get(url, stream=True)
+                r.raise_for_status()
+                shutil.copyfileobj(r.raw, f)
+
+    def _validate_file(self, file):
+        if 'name' not in file:
+            raise ValueError(f'name is not defined: {file}')
+        value = file['name']
+        if not isinstance(value, str):
+            raise ValueError(f'name is not str: {value}')
+        if 'type' not in file:
+            raise ValueError(f'type is not defined: {file}')
+        value = file['type']
+        if not isinstance(value, str):
+            raise ValueError(f'type is not str: {value}')
+        if 'importType' not in file:
+            raise ValueError(f'importType is not defined: {file}')
+        value = file['importType']
+        if not isinstance(value, str):
+            raise ValueError(f'importType is not str: {value}')
+        if 'preload' not in file:
+            raise ValueError(f'preload is not defined: {file}')
+        value = file['preload']
+        if not isinstance(value, bool):
+            raise ValueError(f'preload is not str: {value}')
+        if 'priority' not in file:
+            raise ValueError(f'priority is not defined: {file}')
+        value = file['priority']
+        if not isinstance(value, int):
+            raise ValueError(f'priority is not int: {value}')
+
+    def _validate_param(self, param):
+        if param is None:
+            return
+        if not isinstance(param, list):
+            raise ValueError(f'param is not list or none: {param}')
+        for p in param:
+            self._validate_param_field(p)
+
+    def _validate_param_field(self, elem):
+        if 'name' not in elem:
+            raise ValueError(f'name is not defined: {elem}')
+        elemname = elem['name']
+        if not isinstance(elemname, str):
+            raise ValueError(f'name is not str: {elemname}')
+        if 'value' not in elem:
+            raise ValueError(f'value is not defined: {elem}')
+        elemvalue = elem['value']
+        if not isinstance(elemvalue, str):
+            raise ValueError(f'value is not str: {elemvalue}')
+
+    def _validate_paramschema(self, paramschema):
+        if paramschema is None:
+            return
+        if not isinstance(paramschema, list):
+            raise ValueError(f'paramschema is not list or none: {paramschema}')
+        for p in paramschema:
+            self._validate_paramschema_field(p)
+
+    def _validate_paramschema_field(self, elem):
+        types = ['string', 'integer', 'number', 'file', 'boolean', 'url']
+        if 'name' not in elem:
+            raise ValueError(f'name is not defined: {elem}')
+        elemname = elem['name']
+        if not isinstance(elemname, str):
+            raise ValueError(f'name is not str: {elemname}')
+        if 'type' not in elem:
+            raise ValueError(f'type is not defined: {elem}')
+        elemtype = elem['type']
+        if elemtype not in types:
+            raise ValueError(f'unexpected type: {elemtype}')
+
+    def _get_file_attr(self, file):
+        attr = file['data']['attributes']
+        r = {}
+        for key in ['importType', 'name', 'preload', 'priority', 'type']:
+            r[key] = attr[key]
+        return r
+
+class V1API(object):
+    def __init__(self, token):
+        self.token = token
+        if token is None:
+            raise ValueError('token is not set')
+
+    def get_task(self, task_id, shared=False):
+        return self._get('tasks' if not shared else 'shares', task_id)
+
+    def patch_task(self, task_id, task, shared=False):
+        data = {
+            'type': 'tasks',
+            'data': {
+                'attributes': task.serialize_as_task(),
+            },
+        }
+        data_files = task.get_data_files()
+        return self._patch('tasks' if not shared else 'shares', task_id, data, data_files)
+
+    def _get(self, task_type, task_id):
+        logger.info(f'Acquiring for task({task_type}/{task_id})')
+        resp = requests.get(
+            self._get_url(task_type, task_id),
+            headers={'Authorization': f'Bearer {self.token}'},
+        )
+        resp.raise_for_status()
+        respjson = resp.json()
+        logger.info(f'Task acquired({task_type}/{task_id}): {respjson}')
+        return Task(respjson['data']['attributes'])
+
+    def _patch(self, task_type, task_id, data, data_files):
+        logger.info(f'Patch for task({task_type}/{task_id}): {data}')
+        resp = requests.patch(
+            self._get_url(task_type, task_id),
+            json=data,
+            headers={'Authorization': f'Bearer {self.token}'},
+        )
+        respjson = resp.json()
+        logger.info(f'Patched for task({task_type}/{task_id}): {respjson}')
+        resp.raise_for_status()
+        for filename, hash, filepath in data_files:
+            logger.info(f'Uploading {filepath}...')
+            files = [file
+                     for file in respjson['data']['attributes']['files']
+                     if file['data']['attributes']['hashSHA256'] == hash and
+                         file['data']['attributes']['name'] == filename]
+            if len(files) == 0:
+                raise IOError(f'Unexpected result: no entities for {filename}')
+            with open(filepath, 'rb') as f:
+                r = requests.put(
+                    files[0]['links']['upload'],
+                    files={'content': f},
+                )
+                r.raise_for_status()
+        return Task(respjson['data']['attributes'])
+
+    def _get_url(self, task_type, task_id):
+        return f'https://goemon.cloud/api/v1/{task_type}/{task_id}'
---
+++ goemon/cli.py
@@ -0,0 +1,262 @@
+import argparse
+import difflib
+import io
+import logging
+import os
+import shutil
+import sys
+import yaml
+
+from .api import V1API, PROPERTIES
+
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_FILENAMES = {
+    'meta': 'meta.yml',
+    'description': 'description.md',
+    'script': 'script.js',
+    'param': 'param.yml',
+    'paramschema': 'paramschema.yml',
+    'files': 'files/',
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--description',
+    default=None,
+    help='Path to/from which to save/load the description. - means stdin/stdout',
+)
+parser.add_argument(
+    '--script',
+    default=None,
+    help='Path to/from which to save/load the script. - means stdin/stdout',
+)
+parser.add_argument(
+    '--param',
+    default=None,
+    help='Path to/from which to save/load the parameters. - means stdin/stdout',
+)
+parser.add_argument(
+    '--paramschema',
+    default=None,
+    help='Path to/from which to save/load the parameter schema. - means stdin/stdout',
+)
+parser.add_argument(
+    '--meta',
+    default=None,
+    help='Path to/from which to save/load the metadata. - means stdin/stdout',
+)
+parser.add_argument(
+    '--files',
+    default=None,
+    help='Path to/from which to save/load the files. - means stdin/stdout',
+)
+parser.add_argument(
+    '--all',
+    action='store_const',
+    const=True,
+    default=False,
+    help='Import/export all types of data. If no filename is specified, use default',
+)
+parser.add_argument(
+    '--base-dir',
+    '-d',
+    default='.',
+    help='Base directory',
+)
+parser.add_argument(
+    '--overwrite',
+    action='store_const',
+    const=True,
+    default=False,
+    help='Specify if overwrite files',
+)
+parser.add_argument(
+    '--dry-run',
+    action='store_const',
+    const=True,
+    default=False,
+    help='Specify if check mode',
+)
+parser.add_argument(
+    '--shared',
+    action='store_const',
+    const=True,
+    default=False,
+    help='Specify if the task is Shared Task',
+)
+parser.add_argument(
+    '--verbose',
+    '-v',
+    action='store_const',
+    const=True,
+    default=False,
+    help='Verbose output',
+)
+parser.add_argument(
+    'subcommand',
+    metavar='subcommand',
+    nargs=1,
+    help='import|export',
+)
+parser.add_argument(
+    'target_id',
+    metavar='target_id',
+    nargs=1,
+    help='Task ID/Shared ID',
+)
+
+def get_stream(args, dest, mode, stdstream):
+    if dest == '-':
+        return stdstream
+    path = os.path.join(args.base_dir, dest)
+    if mode == 'w' and os.path.exists(path) and not args.overwrite:
+        raise ValueError(f'File already exists: {path}')
+    return open(path, mode)
+
+def process_import(args, task, prop):
+    value = getattr(task, prop)
+    dest = getattr(args, prop)
+    if args.all and dest is None:
+        dest = DEFAULT_FILENAMES[prop]
+    if dest is None:
+        return
+    with get_stream(args, dest, 'w', sys.stdout) as stream:
+        stream.write(value)
+
+def process_files_import(args, task):
+    value, files = task.files
+    dest_dir = args.files
+    if dest_dir is not None:
+        dest_dir = os.path.join(args.base_dir, dest_dir)
+    elif args.all and dest_dir is None:
+        dest_dir = os.path.join(args.base_dir, DEFAULT_FILENAMES['files'])
+    if dest_dir is None:
+        return
+    os.makedirs(dest_dir, exist_ok=True)
+    files_yaml = os.path.join(dest_dir, '.files.yml')
+    if os.path.exists(files_yaml) and not args.overwrite:
+        raise ValueError(f'File already exists: {files_yaml}')
+    with open(files_yaml, 'w') as stream:
+        stream.write(value)
+    for src_dir, filepath in files:
+        logger.info(f'Copy {filepath}, {src_dir} -> {dest_dir}')
+        dest_path = os.path.join(dest_dir, filepath)
+        if os.path.exists(dest_path) and not args.overwrite:
+            raise ValueError(f'File already exists: {dest_path}')
+        dest_path_dir, _ = os.path.split(dest_path)
+        os.makedirs(dest_path_dir, exist_ok=True)
+        shutil.copyfile(os.path.join(src_dir, filepath), dest_path)
+
+def process_export(args, task, prop):
+    src = getattr(args, prop)
+    filename = DEFAULT_FILENAMES[prop]
+    if args.all and src is None:
+        src = filename
+    if src is None:
+        return
+    with get_stream(args, src, 'r', sys.stdout) as stream:
+        value = stream.read()
+    if args.dry_run:
+        oldvalue = getattr(task, prop)
+        sys.stdout.writelines(difflib.unified_diff(
+            io.StringIO(oldvalue).readlines(),
+            io.StringIO(value).readlines(),
+            f'a/{filename}',
+            f'b/{filename}',
+        ))
+    setattr(task, prop, value)
+
+def process_files_export(args, task):
+    src_dir = args.files
+    src_path = src_dir
+    if src_dir is not None:
+        src_dir = os.path.join(args.base_dir, src_dir)
+    elif args.all and src_dir is None:
+        src_dir = os.path.join(args.base_dir, DEFAULT_FILENAMES['files'])
+        src_path = DEFAULT_FILENAMES['files']
+    if src_dir is None:
+        return
+    files_yaml_path = os.path.join(src_dir, '.files.yml')
+    with open(files_yaml_path, 'r') as stream:
+        files_yaml = stream.read()
+    files_path = []
+    for file in yaml.load(files_yaml, yaml.SafeLoader):
+        filename = file['name']
+        if not os.path.exists(os.path.join(src_dir, filename)):
+            raise ValueError(f'File not found: {src_dir}')
+        files_path.append((src_dir, filename))
+    if args.dry_run:
+        oldvalue, oldfiles_path = task.files
+        sys.stdout.writelines(difflib.unified_diff(
+            io.StringIO(oldvalue).readlines(),
+            io.StringIO(files_yaml).readlines(),
+            f'a/.files.yml',
+            f'b/.files.yml',
+        ))
+        newfilenames = set([filename for _, filename in files_path])
+        oldfilenames = set([filename for _, filename in oldfiles_path])
+        filenames = sorted(list(newfilenames | oldfilenames))
+        for filename in filenames:
+            if filename in newfilenames and filename not in oldfilenames:
+                sys.stdout.writelines([f'New file - b/{src_path}/{filename}\n'])
+                continue
+            if filename not in newfilenames and filename in oldfilenames:
+                sys.stdout.writelines([f'Deleted file - a/{src_path}/{filename}\n'])
+                continue
+            file = os.path.join(src_dir, filename)
+            oldfile = os.path.join(
+                [oldfile_dir for oldfile_dir, oldfilename in oldfiles_path if oldfilename == filename][0],
+                filename,
+            )
+            with open(oldfile, 'rb') as oldf:
+                with open(file, 'rb') as f:
+                    lines = difflib.diff_bytes(
+                        difflib.unified_diff,
+                        oldf.readlines(),
+                        f.readlines(),
+                        f'a/{src_path}/{filename}'.encode('utf8'),
+                        f'b/{src_path}/{filename}'.encode('utf8'),
+                    )
+                    try:
+                        sys.stdout.writelines([l.decode('utf8') for l in lines])
+                    except UnicodeDecodeError:
+                        sys.stdout.writelines(
+                            [f'Binary files changed - a/{src_path}/{filename}, b/{src_path}/{filename}\n']
+                        )
+    task.files = (files_yaml, files_path)
+
+def flush_export(args, api, task):
+    if args.dry_run:
+        logger.info('Finished: check-mode')
+        return
+    api.patch_task(args.target_id[-1], task, shared=args.shared)
+
+def main():
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.WARN,
+    )
+    token = os.environ.get('GOEMON_TOKEN', None)
+    if token is None:
+        raise ValueError('Environment variable GOEMON_TOKEN missing')
+    api = V1API(token=token)
+    task = api.get_task(args.target_id[-1], shared=args.shared)
+    if all([getattr(args, prop) is None for prop in PROPERTIES]) and args.files is None and not args.all:
+        raise ValueError('No files specified')
+    if len([prop for prop in PROPERTIES if getattr(args, prop) == '-']) > 1:
+        raise ValueError('"-" cannot be set more than once')
+    for prop in PROPERTIES:
+        if args.subcommand[0] == 'import':
+            process_import(args, task, prop)
+        elif args.subcommand[0] == 'export':
+            process_export(args, task, prop)
+        else:
+            raise ValueError(f'Unexpected subcommand: {args.subcommand[0]}')
+    if args.subcommand[0] == 'import':
+        process_files_import(args, task)
+    elif args.subcommand[0] == 'export':
+        process_files_export(args, task)
+        flush_export(args, api, task)
+    task.destroy()
